06db051f1680dcfa44680c06c68228187ceecc3c
Jus Jassar <jusjassar@gmail.com>
2023-07-13 18:15:28 -0400

    vedio16

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavComponent } from './nav/nav.component';
 import { HeaderComponent } from './header/header.component';
 import { NotificationComponent } from './notification/notification.component';
 import { FormsModule } from '@angular/forms';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { FormsModule } from '@angular/forms';
     NavComponent,
     HeaderComponent,
     NotificationComponent,
+    SearchComponent,
   ],
   imports: [
     BrowserModule,
