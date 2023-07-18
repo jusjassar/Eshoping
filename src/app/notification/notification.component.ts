b3d12968e92d4a703b688398849d1d68ecb2eb83
Jus Jassar <jusjassar@gmail.com>
2023-07-17 22:11:03 -0400

    prep

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -2,8 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
-  template: `<div class="alert alert-success"> 
+  template: `<div class="alert alert-success" [hidden]="displayNotification"> 
                 This website uses cookies to provide better user experience.
+                <div class="close">
+                  <button class="btn" (click)="notificationClose()">X</button>
+
+                </div>
               </div>`,
   styles: ["div{margin: 10px 0px; padding: 10px 20px; text-align:center;}", 
           "p{font-size: 14px;}", ".close{float: right; margin-top: -15px;}",
@@ -17,7 +21,7 @@ export class NotificationComponent implements OnInit {
   }
   displayNotification:boolean = false;
 
-  closeNotification(){
+  notificationClose(){
     this.displayNotification = true;
   }
 }
